3046cca5e2e4a0816c0e5b7a61c285e46dff7ab9
AdityaPudasaini <workingaditya00@gmail.com>
2026-05-17 18:49:38 +0545

    Set data in servlet

--- src/main/java/com/controller/UserServlet.java
+++ src/main/java/com/controller/UserServlet.java
@@ -6,6 +6,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import com.dao.AdminUserDAO;
+import com.model.AdminUserModel;
 
 /**
  * Servlet implementation class users
@@ -27,6 +31,25 @@ public class UserServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		try {
+            AdminUserDAO adminUserDao = new AdminUserDAO();
+
+
+
+            ArrayList<AdminUserModel> pagedUsers = new ArrayList<>(allUsers.subList(startFrom, endWith));
+
+            request.setAttribute("users", pagedUsers);
+            request.setAttribute("currentPageInUsers", page);
+            request.setAttribute("totalPages", totalPages);
+            request.setAttribute("selectedStatus", selectedStatus);
+            request.setAttribute("selectedSearch", selectedSearch);
+
+        } 
+		
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+		
 		request.setAttribute("currentPage", "users");
 		request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request, response);
 	}
