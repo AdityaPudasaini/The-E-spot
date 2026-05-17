aade109a228c717864c434e7237561351fe89ccb
jasun-maharjan1 <jasun750@gmail.com>
2026-05-17 21:33:16 +0545

    Added Servlet for About us

--- src/main/java/com/project/controller/AboutServlet.java
+++ src/main/java/com/project/controller/AboutServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 /**
  * Servlet implementation class AboutServlet
  */
-@WebServlet("/aboutus")
+@WebServlet("/aboutUs")
 public class AboutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
