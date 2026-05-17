0f35ddaabb452a33d6caaf116bee3e195d4df48e
AdityaPudasaini <workingaditya00@gmail.com>
2026-05-17 14:10:31 +0545

    Changed servlet for page number

--- src/main/java/com/controller/ListingServlet.java
+++ src/main/java/com/controller/ListingServlet.java
@@ -1,11 +1,13 @@
 package com.controller;
 
 import jakarta.servlet.ServletException;
+import com.model.AdminListingModel;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.dao.AdminListingDAO;
 
@@ -32,16 +34,47 @@ public class ListingServlet extends HttpServlet {
 		
 		 try {
 		        AdminListingDAO adminListingDao = new AdminListingDAO();
-		        request.setAttribute("listings", adminListingDao.allListings());
+		        
+		        int pageSize = 10;
+		        int page = 1;
+		        
+		        String pageString= request.getParameter("page");
+		        
+		        if (pageString != null && !pageString.isEmpty()) {
+		            page = Integer.parseInt(pageString);
+		        }
+		        
+		        ArrayList<AdminListingModel> allListings = adminListingDao.allListings();
+		        
+		        int totalCount = allListings.size();
+		        int totalPages = (totalCount + pageSize - 1) / pageSize;
+		        
+		        int startFrom = (page - 1) * pageSize;
+		        int endWith;
+		        
+		        if (startFrom + pageSize > totalCount) {
+		            endWith = totalCount;
+		        } 
+		        
+		        else {
+		            endWith = startFrom + pageSize;
+		        }
+		        
+		        ArrayList<AdminListingModel> pagedListings = new ArrayList<>(allListings.subList(startFrom, endWith));
+		        
+		        request.setAttribute("listings", pagedListings);
 		        request.setAttribute("categories", adminListingDao.categories());
-		    } 
+		        request.setAttribute("currentPage", page);
+		        request.setAttribute("totalPages", totalPages);
+		        
+		 } 
 		 
 		 catch (Exception e) {
 		        e.printStackTrace();
 		    }
-		
-		request.setAttribute("currentPage", "listing");
-		request.getRequestDispatcher("/WEB-INF/pages/listings.jsp").forward(request, response);
+		    
+		    request.setAttribute("currentPage", "listing");
+		    request.getRequestDispatcher("/WEB-INF/pages/listings.jsp").forward(request, response);
 	}
 
 	/**
