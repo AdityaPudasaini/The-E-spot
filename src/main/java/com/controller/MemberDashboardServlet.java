586f7414d665cd9003a960ce8cdefe7119435eea
AdityaPudasaini <workingaditya00@gmail.com>
2026-05-20 00:40:49 +0545

    Done in do get

--- src/main/java/com/controller/MemberDashboardServlet.java
+++ src/main/java/com/controller/MemberDashboardServlet.java
@@ -1,15 +1,16 @@
 package com.controller;
 
 import jakarta.servlet.ServletException;
+
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
+import jakarta.servlet.http.HttpSession;
 
-import com.dao.MemberDAO;
-import com.model.MemberModel;
+import java.io.IOException;
+import com.dao.UserDashboardDAO;
+import com.model.UserStatsModel;
 
 /**
  * Servlet implementation class MemberDashboard
@@ -32,33 +33,34 @@ public class MemberDashboardServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		String username = (String) request.getSession().getAttribute("username");
-
-        try {
-            MemberDAO memberDao = new MemberDAO();
-            MemberModel member = memberDao.getMemberRecordByUsername(username);
-
-            String joinDate = "";
-            
-            if (member.getCreatedDate() != null) {
-                joinDate = new SimpleDateFormat("MMMM yyyy").format(member.getCreatedDate());
-            }
-
-            String dobFormatted = "";
-            
-            if (member.getDob() != null) {
-                dobFormatted = new SimpleDateFormat("dd MMM yyyy").format(member.getDob());
-            }
-
-            request.setAttribute("memberInfo", member);
-            request.setAttribute("joinDate", joinDate);
-            request.setAttribute("dobFormatted", dobFormatted);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		HttpSession session = request.getSession();
 		
-		request.getRequestDispatcher("/WEB-INF/pages/dashboard.jsp").forward(request, response);
+		int memberId = (int) session.getAttribute("memberId");
+ 
+		try {
+			UserDashboardDAO userDao = new UserDashboardDAO();
+ 
+			UserStatsModel userStats = new UserStatsModel();
+			
+			userStats.setTotalRevenue(userDao.totalRevenue(memberId).getTotalRevenue());
+			userStats.setTotalBought(userDao.totalBought(memberId).getTotalBought());
+			userStats.setTotalSold(userDao.totalSold(memberId).getTotalSold());
+			userStats.setTotalSpent(userDao.totalSpent(memberId).getTotalSpent());
+ 
+			request.setAttribute("userStats", userStats);
+			request.setAttribute("cartData",     userDao.cartItems(memberId));
+			request.setAttribute("recentBought", userDao.recentBought(memberId));
+			request.setAttribute("wishlistData", userDao.wishlistItems(memberId));
+			request.setAttribute("recentSold",   userDao.recentSold(memberId));
+ 
+		} 
+		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+ 
+		request.setAttribute("currentPage", "dashboard");
+		request.getRequestDispatcher("/WEB-INF/pages/userDashboard.jsp").forward(request, response);
 	}
 
 	/**
