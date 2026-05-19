5dc38d984eaedbaeb9671dbb8386230e368183d2
AdityaPudasaini <workingaditya00@gmail.com>
2026-05-19 18:35:41 +0545

    Added a new function in DAO

--- src/main/java/com/dao/AdminReportDAO.java
+++ src/main/java/com/dao/AdminReportDAO.java
@@ -56,5 +56,37 @@ public class AdminReportDAO {
         return date;
     }
 
+    public List<String[]> revenueReport() throws SQLException {
+    	
+        Connection conn = DBConfig.getConnection();
+        
+        String sqlCode = "SELECT p.Payment_ID, p.Payment_Date, p.Payment_Amount, p.Payment_Method, p.Payment_Status FROM payment p WHERE p.Payment_Status = 'Completed' ORDER BY p.Payment_Date DESC";
+        
+        PreparedStatement pst = conn.prepareStatement(sqlCode);
+        ResultSet rs = pst.executeQuery();
+        
+        List<String[]> rows = new ArrayList<>();
+        
+        rows.add(new String[]{"Payment ID", "Date", "Amount", "Method", "Status"});
+        
+        while (rs.next()) {
+        	
+            rows.add(new String[] {
+            	String.valueOf(rs.getInt("Payment_ID")), 
+	            rs.getString("Payment_Date").substring(0, 10),
+	            String.format("%.2f", rs.getDouble("Payment_Amount")),
+	            rs.getString("Payment_Method"),
+	            rs.getString("Payment_Status")
+            	}
+            );
+        }
+        
+        rs.close();
+        pst.close();
+        conn.close();
+        
+        return rows;
+    }
+
     
 }
