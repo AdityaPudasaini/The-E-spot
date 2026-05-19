5531757731c1f8a2d93648bd9db4a35d32103960
AdityaPudasaini <workingaditya00@gmail.com>
2026-05-20 00:11:23 +0545

    Added model

---
+++ src/main/java/com/model/DashboardItemsModel.java
@@ -0,0 +1,71 @@
+package com.model;
+
+public class DashboardItemsModel {
+	
+	private String item1Name;
+    private double item1Price;
+    private String item2Name;
+    private double item2Price;
+    private String item3Name;
+    private double item3Price;
+  
+    public String getItem1Name() 
+    { 
+    	return item1Name; 
+    }
+    
+    public void setItem1Name(String item1Name) 
+    { 
+    	this.item1Name = item1Name; 
+    }
+ 
+    public double getItem1Price() 
+    { 
+    	return item1Price; 
+    }
+    
+    public void setItem1Price(double item1Price) 
+    { 
+    	this.item1Price = item1Price; 
+    }
+ 
+    public String getItem2Name() 
+    { 
+    	return item2Name; 
+    }
+    
+    public void setItem2Name(String item2Name) 
+    { 
+    	this.item2Name = item2Name; 
+    }
+ 
+    public double getItem2Price() 
+    { 
+    	return item2Price; 
+    }
+    
+    public void setItem2Price(double item2Price) 
+    { 
+    	this.item2Price = item2Price; 
+    }
+    
+    public String getItem3Name() 
+    { 
+    	return item3Name; 
+    }
+    
+    public void setItem3Name(String item3Name)      
+    { 
+    	this.item3Name = item3Name; 
+    }
+ 
+    public double getItem3Price()             
+    { 
+    	return item3Price; 
+    }
+    
+    public void setItem3Price(double v)     
+    { 
+    	this.item3Price = v; 
+    }
+}
