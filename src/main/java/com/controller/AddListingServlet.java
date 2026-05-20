06f8f5315ce0b65890a63bab7341430714f95838
AdityaPudasaini <workingaditya00@gmail.com>
2026-05-20 07:28:08 +0545

    Added in do post

--- src/main/java/com/controller/AddListingServlet.java
+++ src/main/java/com/controller/AddListingServlet.java
@@ -6,8 +6,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
 import java.io.IOException;
 import java.util.ArrayList;
+
+import com.utils.FileUploadUtil;
 
 /**
  * Servlet implementation class AddListingServlet
@@ -56,7 +60,46 @@ public class AddListingServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		try {
+            String productName = request.getParameter("productName");
+            String productDescription = request.getParameter("productDescription");
+            String productPrice = request.getParameter("productPrice");
+            String stockQuantity = request.getParameter("stockQuantity");
+            String categoryId = request.getParameter("categoryId");
+            String sellerUsername = (String) request.getSession().getAttribute("username");
+            
+ 
+            if (productName == null || productName.trim().isEmpty() ||
+                productDescription == null || productDescription.trim().isEmpty() ||
+                productPrice == null || categoryId == null || categoryId.isEmpty()) {
+                request.setAttribute("errorMessage", "Please fill in all fields.");
+                doGet(request, response);
+                return;
+            }
+            
+ 
+            AddListingDAO addListingDao = new AddListingDAO();
+            
+            int productId = addListingDao.addListing(productName, productDescription, Double.parseDouble(productPrice), Integer.parseInt(stockQuantity), Integer.parseInt(categoryId), sellerUsername);
+            
+            Part filePart = request.getPart("Photo");
+	        
+	        if (filePart != null && filePart.getSize() > 0) 
+	        {
+	            String extension = FileUploadUtil.getFileExtension(filePart.getSubmittedFileName());
+	            String fileName = "product" + productId;
+	            FileUploadUtil.saveFile(filePart, UPLOAD_DIR, fileName);
+	        }
+ 
+            response.sendRedirect(request.getContextPath() + "/userDashboard");
+ 
+        } 
+		
+		catch (Exception e) {
+            e.printStackTrace();
+            request.setAttribute("errorMessage", e.getMessage());
+            doGet(request, response);
+        }
 	}
 
 }
